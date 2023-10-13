db89d287ab9f7b0a3a77ee1abcfef269edb6862c
wochowskaj83 <wochowskaj@gmail.com>
2023-10-13 14:29:11 +0200

    instalacja i polaczenie z mongoose

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const hbs = require('express-handlebars');
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/express-zad47147");");
 
 app.engine("hbs", hbs.engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 
 app.get('/', function(req, res){
-    res.render("home");
+    res.render("home", {title: 'My app title', content: "Lorem ipsum"});
 });
 
 app.listen(5500, function(){
